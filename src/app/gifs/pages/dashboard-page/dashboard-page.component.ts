5232de8e668cb6ec719abcb796c850e70f09673a
Alexis Peralta <l.pera040904@itses.edu.mx>
2025-10-15 22:25:07 -0600

    Componente con link

---
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SideMenuComponent } from '../../components/side-menu/side-menu.component';
+@Component({
+  selector: 'app-dashboard',
+  imports: [RouterOutlet, SideMenuComponent],
+  templateUrl: './dashboard-page.component.html',
+  styleUrl: './dashboard-page.component.scss'
+})
+export class DashboardComponent {
+
+}
